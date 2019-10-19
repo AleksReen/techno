f50858f6d6951eae9c829dd7bab9ec6fb6ce8f45
AleksReen <aleksandr.reentovich@tut.by>
2019-10-19 19:28:48 +0300

    release

--- js/common.js
+++ js/common.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-    $("#header-menu, #toTop, #section_mounting, #section_electrical, #section_painting, #section_roofing")
+    $("#header-menu, #toTop, #section_mounting, #section_electrical, #section_painting, #section_roofing, #section_wood, #section_rigging, #section_welding, #section_other")
     .on("click", ".anchor", function(event) {
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({
             scrollTop: top
-        }, 500);
+        }, 700);
+    });
+});
+
+
+$(function() {
+    $('.header__conteiner-link a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $('.navbar-toggle').trigger( "click" );
+        }
     });
 });
